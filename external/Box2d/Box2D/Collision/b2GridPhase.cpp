e0eb06e0a0cd0d8c6038e2fff8396ad158162ed7
zhaoxm <zhaoxm222@gmail.com>
2013-01-11 01:28:51 +0800

    grid phase

--- external/Box2d/Box2D/Collision/b2GridPhase.cpp
+++ external/Box2d/Box2D/Collision/b2GridPhase.cpp
@@ -6,4 +6,16 @@
 //
 //
 
-#include "b2GridPhase.h"
+#include <Box2D/Collision/b2GridPhase.h>
+
+
+b2GridPhase::b2GridPhase( uint32 width, uint32 height )
+{
+    nWidth_ = width;
+    nHeight_ = height;
+    
+    collisionData_ = (uint8*)b2Alloc( DEFAULT_GRID_COLLISION_COUNT * GRID_COLLISION_SIZE * GRID_COLLISION_SIZE * sizeof( uint8 ) );
+    grids_ = (uint32*)b2Alloc( nWidth_ * nHeight_ * sizeof(uint32) );
+    gridData_ = NULL;
+    gridDataSize_ = 0;
+}
--- external/Box2d/Box2D/Collision/b2GridPhase.h
+++ external/Box2d/Box2D/Collision/b2GridPhase.h
@@ -9,27 +9,52 @@
 #ifndef B2_GRID_PHASE_H
 #define B2_GRID_PHASE_H
 
+#include <Box2D/Common/b2Settings.h>
 #include <Box2D/Common/b2Math.h>
+
+#define GRID_COLLISION_SIZE 16
+#define DEFAULT_GRID_COLLISION_COUNT 1000
+
+struct b2Grid
+{
+    /// collision index , reference to b2GridPhase collisionData
+    uint32 collisionIdx_;
+    
+    /// next grid, linked as a shape
+    b2Grid* next_;
+    
+    /// user data
+    void*  userData;
+};
+
 
 class b2GridPhase
 {
 public:
-    b2GridPhase( float width, float height, float gridSize, int gridCells );
+    b2GridPhase( uint32 width, uint32 height );
     
+    void SetPosition( b2Vec2& pos );
     
 private:
+    /// grid phase left top pos
+    b2Vec2 pos_;
     
-    b2Vec2  m_position;     // grid phase left top pos
+    /// grid phase area
+    uint32 nWidth_, nHeight_;
     
-    float m_width;          // grid area , in meters
-    float m_height;
-    int m_nWidth;           // grid area , in grids
-    int m_nHeight;
-    
-    float m_gridSize;       // grid size , in meters
-    
-    int m_gridCells;        // split grid by cells
-    float m_cellSize;       
+    /// collision data
+    uint8* collisionData_;
+
+    /// grids matrix
+    uint32* grids_;
+    b2Grid* gridData_;
+    uint32 gridDataSize_;
+};
+
+
+inline void b2GridPhase::SetPosition( b2Vec2& pos )
+{
+    pos_ = pos;
 }
 
 
